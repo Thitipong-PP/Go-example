b5a585bccf3998bf07cce4581d1f1f9bc6456663
ThitipongPP <thitipongpp@MacBook-Air-khxng-ThitipongPP.local>
2026-02-02 10:59:06 +0700

    Try array and slice

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	testpackage "github.com/Thitipong-PP/Go-example/test_package"
 
 	"github.com/google/uuid"
@@ -35,12 +37,26 @@ func main () {
 	// }
 
 	// Do-While loop
-	i := 0
-	for {
-		println(i)
-		i++;
-		if i >= 10 {
-			break
-		}
-	}
+	// i := 0
+	// for {
+	// 	println(i)
+	// 	i++;
+	// 	if i >= 10 {
+	// 		break
+	// 	}
+	// }
+
+	var arr [3]int
+	arr[0] = 1
+	arr[1] = 9
+	arr[2] = 7
+
+	var sld []int;
+
+	sld = arr[:]
+	sld = append(sld, 8)
+	sld = append(sld, 91821, 8123, 12312312, 12838641)
+
+	fmt.Println(arr)
+	fmt.Println(sld)
 }
